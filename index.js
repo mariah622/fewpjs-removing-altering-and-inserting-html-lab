37596660ad03a4a32bbeda69e723634d7cf2527e
mariah622 <mjustine622@gmail.com>
2021-12-08 19:03:06 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+// Write your code here
+document.getElementById('main').remove()
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory'
+newHeader.innerHTML = "Mariah is the champion"
+
